260ef9f3b2323cfcf567d9570b59b82b20add641
Victor <kminvictor@gmail.com>
2018-07-19 23:19:19 +0200

    Do search and correct /help

--- botApp.js
+++ botApp.js
@@ -1,21 +1,20 @@
 //TODO Пофіксити пошук !!!!!!!!
 //TODO розкидати все по хелпер класах
 //TODO зробити валідації
-//TODO зробити нормальний опис /help.
 //TODO написати коменти
 
 // require my token, bot API, mongoose
-let token = require('./token');
-let taskSchema = require('./model/taskModel');
-let telegramBot = require('node-telegram-bot-api');
-let mongoose = require('mongoose');
+const token = require('./token');
+const taskSchema = require('./model/taskModel');
+const telegramBot = require('node-telegram-bot-api');
+const mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/sPlanerBase');
 
 //create new Bot with token and polling (socket.io analog (google write this ;) ))
-let bot = new telegramBot(token.getToken, {polling: true});
+const bot = new telegramBot(token.getToken, {polling: true});
 
 
 bot.onText(/\/start/, function (msg, match) {
@@ -23,7 +22,18 @@ bot.onText(/\/start/, function (msg, match) {
 });
 
 bot.onText(/\/help/, function (msg, match) {
-    bot.sendMessage(msg.chat.id, 'В церкві поможуть');
+    bot.sendMessage(msg.chat.id, 'Привіт. Це бот-нагадувалка. \n ' +
+        'Працює за дуже простим принципом. \n ' +
+        'ПРосто скажи йому коли і що нагадати, та він напише тобі про цю подію чітко в назначений час \n' +
+        'Що б щось записати в календарик, просто напищи стрічку \n ' +
+        '/save Опис події # Рік-Місяць-День Година:Хвилина. \n' +
+        'Наприклад \n' +
+        '\n' +
+        '/save Винести мусор # 2018-07-18 15:30 \n' +
+        '\n' +
+        '\n' +
+        '\n' +
+        'Автор бота @victor_fzs. Спеціально для компанії UkrInSoft');
 });
 
 bot.onText(/\/save (.+)/, function (msg, match) {
@@ -36,10 +46,9 @@ bot.onText(/\/save (.+)/, function (msg, match) {
     let description = fullMsg.split('#')[0] ? fullMsg.split('#')[0] : '';
     let date = fullMsg.split('#')[1] ? fullMsg.split('#')[1].trim() : '';
 
-    date = new Date(date);
-    date.setHours(date.getHours() - 2);
-
     if (description && date) {
+        date = new Date(date);
+        date.setUTCHours(date.getUTCHours() - 2);
 
         let task = new taskSchema({
             chatId: chatId,
@@ -48,20 +57,16 @@ bot.onText(/\/save (.+)/, function (msg, match) {
         });
         task.save(function (err, taskToSave) {
             if (err) {
-                bot.sendMessage(chatId, 'Якась помилка, краще введи /help.')
+                bot.sendMessage(chatId, 'Якась помилка, краще введи /help.');
                 console.log(err);
             } else {
 
-                bot.sendMessage(chatId, 'Я все зробив, босс');
+                bot.sendMessage(chatId, 'Я все записав, босс');
                 bot.sendMessage(chatId, description + '    опис');
-                bot.sendMessage(chatId, fullMsg.split('#')[1].trim() + '    тіпа дата');
+                bot.sendMessage(chatId, fullMsg.split('#')[1].trim() + '     дата');
             }
         })
     }
-
-    console.log(chatId);
-    console.log(description);
-    console.log(date);
 });
 
 bot.onText(/\/find/, function (msg, match) {
@@ -69,58 +74,41 @@ bot.onText(/\/find/, function (msg, match) {
 });
 
 
-// TODO тимчасовий код. Пофіксити і забрати
 (function () {
-    let now = new Date();
-    now.setHours(now.getHours() + 2);
-    console.log(now);
-        let tasks = taskSchema.find({
+
+    let interval = setInterval(function () {
+        let lteDate = new Date();
+        lteDate.setUTCHours(lteDate.getUTCHours() + 2);
+        console.log(lteDate);
+
+        let gteDate = new Date();
+        gteDate.setUTCHours(gteDate.getUTCHours() + 2);
+        // gteDate.setUTCMinutes(gteDate.getUTCMinutes() - 5);
+        console.log(gteDate);
+
+       let query = taskSchema.find({
             checked: false,
-            data: {
-                $lte: now
+            dateOfTask: {
+                // $gt: gteDate
+                // ,
+                $lt: lteDate
+            }
+        }, function (err, docs) {
+            if (err) {
+                console.log(err)
+            } else {
+                for (let i = 0; i < docs.length; i++) {
+
+                    console.log(docs[i]);
+                    docs[i].checked = true;
+                    docs[i].save();
+                    bot.sendMessage(docs[i].chatId, 'Хазяїн. Нагадую про ' + docs[i].description);
+                    console.log(docs[i].description);
+                    console.log(docs[i].data);
+                }
             }
         });
-        console.log(tasks)
-    }
-)();
-//TODO Кінцеь тимчасового
-
-(function () {
-    // noinspection JSAnnotator
-    let interval = setInterval(function () {
-        let now = new Date();
-        now.setHours(now.getHours() + 2);
-        console.log(now);
-        try {
-            let tasks = taskSchema.find({
-                checked: false,
-                data: {
-                    $lte: now
-                }
-            });
-
-
-            // console.log(tasks);
-
-            Array.prototype.forEach.call(tasks, task => {
-                console.log(task);
-                task.checked = true;
-                task.save();
-                bot.sendMessage(task.chatId, 'Хазяїн. Нагадую про ' + task.description + task.data);
-            });
-
-            // tasks.for(function (task) {
-            //     task.checked = true;
-            //     task.save();
-            //     bot.sendMessage(task.chatId, 'Хазяїн. Нагадую про ' + task.description + task.data);
-            // })
-
-        } catch (e) {
-            console.log(e);
-            // bot.sendMessage(task, 'Cant find tasks')
-        }
-
-    }, 1000)
+    }, 3000)
 })();
 
 // { '0':
